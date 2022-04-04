6f604c0e54a785334b9b9e25c633a34b34dae3a9
Stephcodess <Kelvineddyimache@gmail.com>
2022-04-04 07:11:59 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import { useEffect } from 'react';
 AOS.init();
 
 function App() {
+  useEffect(()=>{
+    fetch("https://fidpress-api.herokuapp.com/").then((res)=>{return res.json()}).then((me)=>console.log(me))
+  },[])
   return (
     <Provider store={store}>
       <AppNavContainer />
